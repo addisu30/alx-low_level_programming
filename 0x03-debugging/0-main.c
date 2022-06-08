29a17dadc6bcb25dc96bc5247a34f6f8a5339219
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-06-08 20:27:08 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*main - main
+*main - tests function that prints if integer is positive or negative
 *Return: 0
 */
 
@@ -12,6 +12,5 @@ i = 0;
 positive_or_negative(i);
 
 return (0);
-
 }
 
