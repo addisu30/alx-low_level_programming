2b18ce0ffc462751af33fa02dd5305add6a1bdc1
addisu30 <dabaleaddisu21@outlook.com>
2022-06-10 02:58:22 +0300

    drawing straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: Determines how long the line will be
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int i;
+
+if (n < 1)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+
+_putchar('\n');
+}
+}
+
