c79862cbf93c1850a0af2e80dc2469099a6c6fd7
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-06-10 03:23:00 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_line - prints a line
+ * print_diagonal - prints a diagonal line
  * @n: Determines how long the line will be
  *
- * Return: Always 0.
+ * Return: On success 0.
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
-int i;
+int i, j;
 
 if (n < 1)
 {
@@ -18,7 +18,19 @@ else
 {
 for (i = 1; i <= n; i++)
 {
-_putchar('_');
+if ((n > 1) && (i > 1))
+{
+for (j = 1; j <= i - 1; j++)
+{
+_putchar(32);
+}
+}
+_putchar('\\');
+if (i != n)
+{
+_putchar('\n');
+}
 }
 _putchar('\n');
+}
 }
