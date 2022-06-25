bbade675a71a9d797dc2e78d1800574468e11f8d
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-06-25 15:20:29 +0300

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,20 +1,99 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
 int _putchar(char c);
+#endif
+
+#ifndef _ISLOWER_H
+#define _ISLOWER_H
 int _islower(int c);
+#endif
+
+#ifndef _ISALPHA_H
+#define _ISALPHA_H
 int _isalpha(int c);
+#endif
+
+#ifndef _ABS_H
+#define _ABS_H
 int _abs(int n);
+#endif
+
+#ifndef _ISUPPER_H
+#define _ISUPPER_H
 int _isupper(int c);
+#endif
+
+#ifndef _ISDIGIT_H
+#define _ISDIGIT_H
 int _isdigit(int c);
+#endif
+
+#ifndef _STRLEN_H
+#define _STRLEN_H
 int _strlen(char *s);
+#endif
+
+#ifndef _PUTS_H
+#define _PUTS_H
 void _puts(char *s);
+#endif
+
+#ifndef _STRCPY_H
+#define _STRCPY_H
 char *_strcpy(char *dest, char *src);
+#endif
+
+#ifndef _ATOI_H
+#define _ATOI_H
 int _atoi(char *s);
+#endif
+
+#ifndef _STRCAT_H
+#define _STRCAT_H
 char *_strcat(char *dest, char *src);
+#endif
+
+#ifndef _STRNCAT_H
+#define _STRNCAT_H
 char *_strncat(char *dest, char *src, int n);
+#endif
+
+#ifndef _STRNCPY_H
+#define _STRNCPY_H
 char *_strncpy(char *dest, char *src, int n);
+#endif
+
+#ifndef _STRCMP_H
+#define _STRCMP_H
 int _strcmp(char *s1, char *s2);
+#endif
+
+#ifndef _MEMSET_H
+#define _MEMSET_H
 char *_memset(char *s, char b, unsigned int n);
+#endif
+
+#ifndef _MEMCPY_H
+#define _MEMCPY_H
 char *_memcpy(char *dest, char *src, unsigned int n);
+#endif
+
+#ifndef _STRCHR_H
+#define _STRCHR_H
 char *_strchr(char *s, char c);
+#endif
+
+#ifndef _UNSIGNED INT _STRSPN_H
+#define _UNSIGNED INT _STRSPN_H
 unsigned int _strspn(char *s, char *accept);
+#endif
+
+#ifndef _STRPBRK_H
+#define _STRPBRK_H
 char *_strpbrk(char *s, char *accept);
+#endif
+
+#ifndef _STRSTR_H
+#define _STRSTR_H
 char *_strstr(char *haystack, char *needle);
+#endif
