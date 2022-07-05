ec69aba0d5d4bd7d94451d21b0f833191cffcaef
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-07-05 14:19:21 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,23 @@
 #include "dog.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
- * init_dog - writes the character c to stdout
- * @d: Dog
- * @name: Dog name
- * @age: dog age
- * @owner: Dog owner
+ * print_dog - prints a struct dog
+ * @d: Pointer to struct
  *
  * Return: Always 0.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
 if (d != 0)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+if ((*d).name != 0)
+printf("Name: %s\n", (*d).name);
+else
+printf("Name: (nil)\n");
+printf("Age: %f\n", (*d).age);
+if ((*d).owner != 0)
+printf("Owner: %s\n", (*d).owner);
+else
+printf("Owner: (nil)\n");
 }
 }
-
