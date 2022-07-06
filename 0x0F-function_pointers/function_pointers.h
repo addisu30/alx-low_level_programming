54d15b7c5e492cd55f28d327a299adf1dc1297df
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-07-06 12:26:13 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef FUNCTIONS_H;
+#define FUNCTIONS_H;
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
