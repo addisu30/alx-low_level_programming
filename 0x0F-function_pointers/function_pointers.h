41aac945eccb328c7ddf9fc1ac86b7ed57200b8d
addisu30 <105362027+addisu30@users.noreply.github.com>
2022-07-06 12:21:44 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
 ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-typedef __SIZE_TYPE__ size_t;
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
